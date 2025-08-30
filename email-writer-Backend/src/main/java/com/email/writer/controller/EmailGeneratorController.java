422e42b0310776289c7adc8f08a34c2567a481f6
Ashutosh <ashutoshbhardwaj257@gmail.com>
2025-08-30 19:43:34 +0530

    update

--- email-writer-Backend/src/main/java/com/email/writer/controller/EmailGeneratorController.java
+++ email-writer-Backend/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class EmailGeneratorController {
 
     @Autowired
-    private EmailGeneratorService emailGeneratorService;
+    private static EmailGeneratorService emailGeneratorService;
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) throws JsonProcessingException {
