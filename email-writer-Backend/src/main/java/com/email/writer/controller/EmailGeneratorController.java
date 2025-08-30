5e59b449d716fcbe9f26e51d5cf941e5bae5796b
Ashutosh <ashutoshbhardwaj257@gmail.com>
2025-08-30 19:44:16 +0530

    update

--- email-writer-Backend/src/main/java/com/email/writer/controller/EmailGeneratorController.java
+++ email-writer-Backend/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
-    @Autowired
+    
     private static EmailGeneratorService emailGeneratorService;
 
     @PostMapping("/generate")
